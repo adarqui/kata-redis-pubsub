f60952ab170542a0402da902468827b80c2145e1
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-15 01:19:30 -0400

    added a sigint handler and some print's, also a try catch on sub

--- python/python.py
+++ python/python.py
@@ -1,15 +1,28 @@
 import redis
-import string
-from types import *
+import sys
+import signal
+
+def signal_handler(signal, frame):
+	print "python: Goodbye."
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 pub = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
 
 r = redis.Redis(host='127.0.0.1',port=6379,db=0)
-sub = r.pubsub()
-sub.subscribe(['ping','vping'])
+
+try:
+	sub = r.pubsub()
+	sub.subscribe(['ping','vping'])
+except:
+	print "python: [sub] Unable to subscribe."
+	sys.exit(-1)
+
 
 while True:
 		packet = next(sub.listen())
+		print "python: Received packet =>", packet
 		channel = packet['channel']
 		if channel == "ping":
 				pub.publish('pong', packet['data'])
